ace05ba6668c2808ecfa834ca7915e92fdd18c12
Divyadarshini <62785267+divpawar@users.noreply.github.com>
2024-07-09 07:52:12 +0000

    Add files via upload

---
+++ src/main/java/com/nokia/demo_divya/DemoDivyaApplication.java
@@ -0,0 +1,13 @@
+package com.nokia.demo_divya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDivyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDivyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nokia/demo_divya/DemoDivyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nokia.demo_divya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDivyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
